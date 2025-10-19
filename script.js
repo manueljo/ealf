638864062b7be27625a4581adf75dd25bf08c5b0
manueljo <mikebillyy@gmail.com>
2025-10-19 14:02:46 +0100

    layout change

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+import { CountUp } from './countUp.min.js';
+
+const countUp = new CountUp('counter-2', 935684, { enableScrollSpy: true });
+// window.onload = function() {
+//   let countUp = new CountUp('counter-2', 935684);
+//   countUp.start();
+// }
 document.addEventListener('DOMContentLoaded', () => {
+    try {
+        let countUp1 = new CountUp('counter-1', 20, { enableScrollSpy: true });
+        let countUp2 = new CountUp('counter-2', 935684, { enableScrollSpy: true });
+        let countUp3 = new CountUp('counter-3', 1233, { enableScrollSpy: true });
+        let countUp4 = new CountUp('counter-4', 8, { enableScrollSpy: true });
+        countUp1.start();
+        countUp2.start();
+        countUp3.start();
+        countUp4.start();
+    } catch (error) {
+        console.error('Error initializing counters:', error);
+    }
     const mobileNav = document.querySelector('.mobile-nav');
     const mobileNavToggle = document.querySelector('.mobile-toggle');
     const closeNav = document.querySelector('.close-nav');
