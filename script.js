e3caf2c62ccabadb4dd6f40f47a6c21299cedec9
manueljo <mikebillyy@gmail.com>
2025-10-25 19:18:44 +0100

    Add about page

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 import { CountUp } from './countUp.min.js';
 
-const countUp = new CountUp('counter-2', 935684, { enableScrollSpy: true });
+// const countUp = new CountUp('counter-2', 935684, { enableScrollSpy: true });
 // window.onload = function() {
 //   let countUp = new CountUp('counter-2', 935684);
 //   countUp.start();
 // }
 document.addEventListener('DOMContentLoaded', () => {
     try {
-        let countUp1 = new CountUp('counter-1', 20, { enableScrollSpy: true });
-        let countUp2 = new CountUp('counter-2', 935684, { enableScrollSpy: true });
-        let countUp3 = new CountUp('counter-3', 1233, { enableScrollSpy: true });
-        let countUp4 = new CountUp('counter-4', 8, { enableScrollSpy: true });
+        let countUp1 = new CountUp('counter-1', 5, { enableScrollSpy: true });
+        let countUp2 = new CountUp('counter-2', 2000, { enableScrollSpy: true });
+        let countUp3 = new CountUp('counter-3', 100, { enableScrollSpy: true });
+        let countUp4 = new CountUp('counter-4', 4, { enableScrollSpy: true });
         countUp1.start();
         countUp2.start();
         countUp3.start();
