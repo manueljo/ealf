a7c84c4dcb2acf88872692f031b1c23bf55ea093
manueljo <mikebillyy@gmail.com>
2025-10-27 16:09:11 +0100

    Add images

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileNavToggle = document.querySelector('.mobile-toggle');
     const closeNav = document.querySelector('.close-nav');
     // const mainNav = document.querySelector('.main-nav');
+    // if (mobileNav.style.display === 'flex'){
+    //      mobileNav.style.display = 'none'
+    // }
 
     mobileNavToggle.addEventListener('click', () => {
         mobileNav.style.display = mobileNav.style.display === 'flex' ? 'none' : 'flex';
